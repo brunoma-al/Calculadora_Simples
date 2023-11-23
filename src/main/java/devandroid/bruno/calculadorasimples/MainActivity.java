4a9d3818089eea4cb36c87f97ef113e811be9607
Bruno Gabriel <brugacag@gmail.com>
2023-11-23 18:38:40 -0300

    FINALIZADO!

--- src/main/java/devandroid/bruno/calculadorasimples/MainActivity.java
+++ src/main/java/devandroid/bruno/calculadorasimples/MainActivity.java
@@ -1,9 +1,7 @@
 package devandroid.bruno.calculadorasimples;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button numeroZero, numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, numeroSeis, numeroSete, numeroOito, numeroNove, ponto, soma, subtrair, multiplicar, dividir, igual, limpar;
 
-    private TextView txtExpressao, txtResultado;
+    private TextView screen;
     private ImageView backspace;
 
     @Override
@@ -26,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         IniciarComponentes();
-        getSupportActionBar();
     }
 
     private void IniciarComponentes() {
@@ -40,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         numeroSete = findViewById(R.id.numero_Sete);
         numeroOito = findViewById(R.id.numero_Oito);
         numeroNove = findViewById(R.id.numero_Nove);
+
         ponto = findViewById(R.id.ponto);
         soma = findViewById(R.id.btn_Somar);
         subtrair = findViewById(R.id.btn_Subtrair);
@@ -47,30 +45,88 @@ public class MainActivity extends AppCompatActivity {
         dividir = findViewById(R.id.btn_Dividir);
         igual = findViewById(R.id.btn_Igual);
         limpar = findViewById(R.id.btn_Limpar);
-        txtExpressao = findViewById(R.id.txt_Expressao);
-        txtResultado = findViewById(R.id.txt_Resultado);
+        screen = findViewById(R.id.screen);
+        backspace = findViewById(R.id.backspace);
 
         limpar.setOnClickListener(view -> {
             firstNum = 0;
-            txtExpressao.setText("0");
+            screen.setText("0");
         });
 
-       // ArrayList<Button>;
+       ArrayList<Button> nums = new ArrayList<>();
+       nums.add(numeroZero);
+       nums.add(numeroUm);
+       nums.add(numeroDois);
+       nums.add(numeroTres);
+       nums.add(numeroQuatro);
+       nums.add(numeroCinco);
+       nums.add(numeroSeis);
+       nums.add(numeroSete);
+       nums.add(numeroOito);
+       nums.add(numeroNove);
 
-    }
 
-    public void AcrescentarUmaExpressao(String string, boolean limpar_dados) {
-        if (txtResultado.getText().equals("")) {
-            txtExpressao.setText("");
-        }
+       for (Button b : nums) {
+           b.setOnClickListener(view -> {
+               if (!screen.getText().toString().equals("0")){
+                   screen.setText(screen.getText().toString() + b.getText().toString());
+               } else {
+                   screen.setText(b.getText().toString());
+               }
+           });
+       }
 
-        if (limpar_dados) {
-            txtResultado.setText("");
-            txtExpressao.append(string);
-        } else {
-            txtExpressao.append(txtResultado.getText());
-            txtExpressao.append(string);
-            txtResultado.setText("");
-        }
+       ArrayList<Button> opers = new ArrayList<>();
+       opers.add(soma);
+       opers.add(subtrair);
+       opers.add(multiplicar);
+       opers.add(dividir);
+       for (Button b : opers) {
+           b.setOnClickListener(view -> {
+               firstNum = Double.parseDouble(screen.getText().toString());
+               operation = b.getText().toString();
+               screen.setText("0");
+           });
+       }
+
+       backspace.setOnClickListener(view -> {
+            String num = screen.getText().toString();
+            if (num.length()>1) {
+                screen.setText(num.substring(0, num.length()-1));
+            } else if (num.length() == 1 && !num.equals("0")) {
+                screen.setText("0");
+            }
+        });
+
+        ponto.setOnClickListener(view -> {
+            if (!screen.getText().toString().contains(".")) {
+                screen.setText(screen.getText().toString() + ".");
+            }
+        });
+
+        igual.setOnClickListener(view -> {
+            double secondNum = Double.parseDouble(screen.getText().toString());
+            double result;
+            switch (operation) {
+                case "/":
+                    result = firstNum/secondNum;
+                    break;
+                case "X":
+                    result = firstNum*secondNum;
+                    break;
+                case "+":
+                    result = firstNum+secondNum;
+                    break;
+                case "-":
+                    result = firstNum-secondNum;
+                    break;
+                default:
+                    result = firstNum+secondNum;
+            }
+            screen.setText(String.valueOf(result));
+            firstNum = result;
+        });
+
+
     }
 }
